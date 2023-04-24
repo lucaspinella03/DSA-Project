e11042df08853657325e2f9d016dee68611cd956
AidanPhilip <48302050+AidanPhilip@users.noreply.github.com>
2023-04-24 14:29:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,6 @@ int main() {
     Simulation sim1;
     //sim1.printInformation();
 
-    sf::RenderWindow mainWindow(sf::VideoMode(640, 480), "SFML Test");
-
-    while(mainWindow.isOpen()){
-
-    }
-
     return 0;
 
 }
